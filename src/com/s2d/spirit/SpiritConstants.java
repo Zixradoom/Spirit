99e12d7253d4611775d2fa8718f8c42b46e37bf5
Anthony J Simon <zixradoom@gmail.com>
2014-09-30 21:55:24 -0400

    finished file move to dir src/main/java from src

--- src/com/s2d/spirit/SpiritConstants.java
+++
@@ -1,8 +0,0 @@
-package com.s2d.spirit;
-
-public final class SpiritConstants
-{
-	public static final String SPIRIT_CHARSET_DEFAUTL = "UTF-8";
-	public static final String SPIRIT_CHARSET = "com.s2d.spirit.charset";
-	public static final long SPIRIT_MAGIC_NUMBER = 0x3040A0B0F0E01070L;
-}
--- src/com/s2d/spirit/event/ServerListener.java
+++
@@ -1,8 +0,0 @@
-package com.s2d.spirit.event;
-
-public interface ServerListener
-{
-	public void onConnect ( ConnectEvent connectEvent );
-	public void onTimeout ( ServerTimeoutEvent serverTimeoutEvent );
-	public void onClose ( ServerCloseEvent serverCloseEvent );
-}
